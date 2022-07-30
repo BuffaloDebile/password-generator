926292ae418d08361c1b6ebed0cc70a503953268
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-07-31 01:54:40 +0200

    creating the password

--- app.js
+++ app.js
@@ -1,23 +1,80 @@
 function getRandomNumber(min, max) {
   let randomNumber = crypto.getRandomValues(new Uint32Array(1))[0];
+
+  // 32 bits = max de 0 à 4 294 967 295
   randomNumber = randomNumber / 4294967296;
+
   return Math.trunc(randomNumber * (max - min + 1)) + min;
 }
 
-console.log(getRandomNumber(0, 5));
-
-function addAsset(fromCode, toCode) {
+function addASet(fromCode, toCode) {
   let charactersList = '';
-  for (let i = fromCode; i <= fromCode; i++) {
+  for (let i = fromCode; i <= toCode; i++) {
     charactersList += String.fromCharCode(i);
   }
   return charactersList;
 }
 
-const characterSet = {
-  lowerCaseChars: addAsset(97, 122),
-  upperCaseChars: addAsset(65, 90),
-  numbers: addAsset(48, 57),
+const charactersSet = {
+  lowercaseChars: addASet(97, 122),
+  upperCaseChars: addASet(65, 90),
+  numbers: addASet(48, 57),
   symbols:
-    addAsset(33, 47) + addAsset(58, 64) + addAsset(91, 96) + addAsset(123, 126),
+    addASet(33, 47) + addASet(58, 64) + addASet(91, 96) + addASet(123, 126),
 };
+
+const passwordContent = document.querySelector('.password-content');
+const errorMsg = document.querySelector('.error-msg');
+const generateBtn = document.querySelector('.generate-password-btn');
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+let passwordLength = 10;
+
+generateBtn.addEventListener('click', createPassword);
+
+function createPassword() {
+  const checkedDataSets = checkedSets();
+
+  if (!checkedDataSets.length) {
+    errorMsg.textContent = 'Au moins une case doit être cochée !';
+    return;
+  } else errorMsg.textContent = '';
+
+  const concatenatedDataSets = checkedDataSets.reduce((acc, cur) => acc + cur);
+
+  let password = '';
+
+  // caractères de base
+  let passwordBase = [];
+  for (let i = 0; i < checkedDataSets.length; i++) {
+    passwordBase.push(
+      checkedDataSets[i][getRandomNumber(0, checkedDataSets[i].length - 1)],
+    );
+  }
+
+  // reste du mdp
+  for (let i = checkedDataSets.length; i < passwordLength; i++) {
+    password +=
+      concatenatedDataSets[getRandomNumber(0, concatenatedDataSets.length - 1)];
+  }
+
+  // mélange
+  passwordBase.forEach((item, index) => {
+    const randomIndex = getRandomNumber(0, password.length);
+    password =
+      password.slice(0, randomIndex) +
+      passwordBase[index] +
+      password.slice(randomIndex);
+  });
+
+  passwordContent.textContent = password;
+}
+createPassword();
+function checkedSets() {
+  const checkedSets = [];
+  checkboxes.forEach(
+    (checkbox) =>
+      checkbox.checked && checkedSets.push(charactersSet[checkbox.id]),
+  );
+
+  return checkedSets;
+}
